2940f68bd1b04fbdd4943150d3ad0c2b9331919d
Ornella <om.bordino@gmail.com>
2015-09-09 23:25:46 -0300

    Se mueven aspects_spec y doman_mock a la carpeta rspec. Se elimina carpeta spec para Travis.

--- rspec/aspects_spec.rb
+++ rspec/aspects_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require_relative '../src/aspects'
-require_relative '../src/domain_mock'
+require_relative '../rspec/domain_mock'
 
 describe 'aspects' do
 
@@ -8,15 +8,10 @@ describe 'aspects' do
 
     a = TestClass.new
     Aspects.on a, /Tes/ do
-      transform(where(namely(/.*crazy.*/))) do
+      transform(where(name(/.*crazy.*/))) do
         create_method
       end
     end
-
-    b = TestClass.new
-    b.extend(TestModule)
-    print b.sarasa
-    print a.sarasa
 
   end
 end
