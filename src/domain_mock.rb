89cfedb219a8ad1936d6b4aca71cc5a4bebcff0c
Federico Cano <canofedericomartin@gmail.com>
2015-09-10 23:48:18 -0300

    correcciones en el test de origin

--- src/domain_mock.rb
+++ src/domain_mock.rb
@@ -14,3 +14,20 @@ class TestClass
 
 end
 
+module Dummy_mixin1
+end
+module Dummy_mixin3
+end
+module Dummy_mixin2
+end
+class Dummy_class1
+end
+class Dummy_class2
+end
+class Dummy_class3
+end
+class Dummy_class4
+end
+class Dummy_class_mixin2
+  extend Dummy_mixin2
+end
