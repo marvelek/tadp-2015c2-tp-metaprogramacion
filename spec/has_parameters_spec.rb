6aba006545206d213475caa10de7b86d05fdd805
Federico Cano <canofedericomartin@gmail.com>
2015-09-14 00:53:41 -0300

    QuantityParameters testeado con Has_Parameters, reflection parece estar funcionando.

--- spec/has_parameters_spec.rb
+++ spec/has_parameters_spec.rb
@@ -73,3 +73,59 @@ describe 'QuantityParameters instance' do
     end
   end
 end
+
+describe 'Has paramaters' do
+  context 'when is used with two parameters and expects 3 parameters' do
+    let (:block_three_parameters) {
+      Aspects.has_parameters(3)
+    }
+    let(:instance) {
+      instance = Dummy_class1.new
+      instance.extend Has_parameters
+    }
+    let (:three_mandatories) {
+      instance.method(:method_3man)
+    }
+    let (:two_mandatories_one_optional) {
+      instance.method(:method_2man_1opt)
+    }
+    let (:one_mandatory_two_optional) {
+      instance.method(:method_1man_2opt)
+    }
+    let (:three_optional) {
+      instance.method(:method_3opt)
+    }
+    let (:two_mandatories) {
+      instance.method(:method_2params)
+    }
+    let (:one_mandatory_one_optional) {
+      instance.method(:method_1man_1opt)
+    }
+    let (:two_optional) {
+      instance.method(:method_2opt)
+    }
+
+    it 'method_3man should be true' do
+      expect(block_three_parameters.call three_mandatories).to be_truthy
+    end
+    it 'method_2man_1opt should be true' do
+      expect(block_three_parameters.call two_mandatories_one_optional).to be_truthy
+    end
+    it 'method_1man_2opt should be true' do
+      expect(block_three_parameters.call one_mandatory_two_optional).to be_truthy
+    end
+    it 'method_3opt should be true' do
+      expect(block_three_parameters.call three_optional).to be_truthy
+    end
+
+    it 'method_2params should be true' do
+      expect(block_three_parameters.call two_mandatories).to be_falsey
+    end
+    it 'method_1man_1opt should be true' do
+      expect(block_three_parameters.call one_mandatory_one_optional).to be_falsey
+    end
+    it 'method_2opt should be true' do
+      expect(block_three_parameters.call two_optional).to be_falsey
+    end
+  end
+end
