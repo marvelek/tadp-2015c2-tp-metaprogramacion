10ded4900016bbcd6ee1822802c30b272b7e63c7
Ornella <om.bordino@gmail.com>
2015-09-18 18:00:41 -0300

    me habia quedado una implementacion va en el otro branch, mil disculpas. Ahi lo saque.

--- src/transformers/redirect.rb
+++ src/transformers/redirect.rb
@@ -1,8 +1,3 @@
 module Redirect
 
-  def redirect_to(replacer_class)
-    @methods.each do |method| method.owner.define_singleton_method(:method) do |*args| replacer_class.method args
-      end
-    end
-  end
 end
