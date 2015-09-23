6b95cbd0f84d51a5d61c9c7c55300c45f96bdfa8
Federico Cano <canofedericomartin@gmail.com>
2015-09-22 21:10:34 -0300

    Revert "Se agregan dos test mostrando: que antes de la modificacion, el metodo, valga la redundancia, no está modificado."
    This reverts commit 2c233691dccaa4dbc293bf2a5b03e4b7b6b7eec7.

--- spec/transf_redirect_spec.rb
+++ spec/transf_redirect_spec.rb
@@ -4,24 +4,13 @@ require_relative '../src/domain_mock'
 
 describe 'Aspects when apply redirect_to' do
 
-  let (:redirect) {
-    Aspects.on Dummy_class5 do
+  it 'should modify the method outside Aspect s block' do
+    Aspects.on Dummy_class2 do
       transform(where name(/saludar/)) do
         redirect_to(Dummy_class3.new)
       end
     end
-  }
 
-  it 'the method should not be modified' do
-    expect(Dummy_class5.new.saludar('Mundo')).to eq 'Hola, Mundo'
-  end
-  
-  it 'should modify the method outside Aspect s block' do
-    redirect
-    expect(Dummy_class5.new.saludar('Mundo')).to eq 'Adiosin, Mundo'
-  end
-
-  it 'the method should be modified forever' do
-    expect(Dummy_class5.new.saludar('Mundo')).to eq 'Adiosin, Mundo'
+    expect(Dummy_class2.new.saludar('Mundo')).to eq 'Adiosin, Mundo'
   end
 end
