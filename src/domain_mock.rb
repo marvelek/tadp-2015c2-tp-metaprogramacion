b011ce2db932bce0885b5e3efce937ad8579b9c2
Marcos Pavelek <marcos.pavelek@gmail.com>
2015-09-26 00:20:00 -0300

    Agregados algunos tests del enunciado

--- src/domain_mock.rb
+++ src/domain_mock.rb
@@ -12,7 +12,7 @@ class TestClass
     "This is a #{p1} method"
   end
 
-  def crazy_long_method(p1,a1,var,var1,lorem2,ipsum2,var6,var9)
+  def crazy_long_method(p1, a1, var, var1, lorem2, ipsum2, var6, var9)
     "This is a #{p1} method"
   end
 
@@ -24,7 +24,7 @@ class TestClassRedirected
     "This is a redirected #{p1} method"
   end
 
-  def crazy_long_method(p1,a1,var,var1,lorem2,ipsum2,var6,var9)
+  def crazy_long_method(p1, a1, var, var1, lorem2, ipsum2, var6, var9)
     "This is a #{p1} method"
   end
 
@@ -63,22 +63,31 @@ end
 class Dummy_class1
   def method_1param(param1)
   end
-  def method_2params(param1,param2)
+
+  def method_2params(param1, param2)
   end
+
   def method_1opt(param1 = nil)
   end
+
   def method_2opt(param1 = nil, param2 = nil)
   end
+
   def method_1man_1opt(param1, param2 = nil)
   end
+
   def method_3man(param1, param2, param3)
   end
+
   def method_2man_1opt(param1, param2, param3 = nil)
   end
+
   def method_1man_2opt(param1, param2 = nil, param3 = nil)
   end
+
   def method_3opt(param1 = nil, param2 = nil, param3 = nil)
   end
+
   def method_2man_2opt(param1, param2, param3 = nil, param4 = nil)
   end
 end
@@ -86,11 +95,14 @@ class Dummy_class2
   def saludar(x)
     "Hola, " + x
   end
-  def method_1arg_with_m(arg1,arg_with_m)
+
+  def method_1arg_with_m(arg1, arg_with_m)
   end
-  def method_with_no_m(arg1,arg2)
+
+  def method_with_no_m(arg1, arg2)
   end
-  def method_with_param(parameter1,parameter2)
+
+  def method_with_param(parameter1, parameter2)
   end
 end
 class Dummy_class3
@@ -98,8 +110,19 @@ class Dummy_class3
     "Adiosin, " + x
   end
 end
+
 class Dummy_class4
 end
+
 class Dummy_class_mixin2
   extend Dummy_mixin2
+end
+
+class MiClase
+  def hace_algo(p1, p2)
+    p1 + '-' + p2
+  end
+  def hace_otra_cosa(p2, ppp)
+    p2 + ':' + ppp
+  end
 end
