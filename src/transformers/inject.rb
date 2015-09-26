053bd7f701ed55e4535fa8f456abfadf726d25fc
Marcos Pavelek <marcos.pavelek@gmail.com>
2015-09-25 21:56:50 -0300

    Fix tests de inject

--- src/transformers/inject.rb
+++ src/transformers/inject.rb
@@ -22,11 +22,10 @@ module Inject
       param_args = parameters_list.zip(original_args).each do |original_param_arg|
         hash.each do |param, arg|
           if original_param_arg.first.equal? param
-            original_param_arg[1] = arg.is_a?(Proc) ? arg.call(self, original_method, original_param_arg.last) : arg
+            original_param_arg[1] = arg.is_a?(Proc) ? arg.call(self, original_method.name, original_param_arg.last) : arg
           end
         end
       end
-
       args = param_args.flat_map(&:last)
       original_method.bind(self).call *args
 
